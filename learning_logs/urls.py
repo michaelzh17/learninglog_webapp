11e31ca48bc3ec1644b53490f500c1dfe9813751
michaelzh17 <macalzhang@gmail.com>
2018-11-11 20:17:16 -0500

    learning log app

---
+++ learning_logs/urls.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+""" Define URL patterns for learning_logs."""
+
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    
+    # Topics page
+    path('topics/', views.topics, name='topics'),
+    
+    # Detail page for a single topic
+    path('topics/<topic_id>', views.topic, name='topic'),
+    
+    # Page for adding a new topic
+    path('new_topic', views.new_topic, name='new_topic'),
+    
+    # Page for adding a new entry
+    path('new_entry/<topic_id>', views.new_entry, name='new_entry'),
+    
+    # Page for editting an entry
+    path('edit_entry/<entry_id>', views.edit_entry, name='edit_entry'),
+]
